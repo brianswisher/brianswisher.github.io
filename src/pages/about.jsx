d72fe259adf6ac01463b9c106257c3624badbeba
Brian Swisher <brian.swisher@syapse.com>
2019-07-31 15:33:40 -0700

    initial commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+export default ({ data }) => (
+  <Layout>
+    <h1>About {data.site.siteMetadata.title}</h1>
+    <p>
+      I’m driven by repeating patterns. Specifically, patterns that are consistently favorable; And easy to repeat. Earlier this decade, the primary pattern consisted of mimicking in the client what we were doing on the server. Model, views, and controllers. The benefit being what feels right; what has less thought friction. A new favorable pattern is emerging. It is component design. It’s different than MVC. It is more guided by a UI than it is of a system. With it comes new concepts that challenge traditional thinking. It has influenced what some are doing on the server. Micro-services more closely align with component design than with monoliths of MVC. By themselves, one is not better than the other. But when compared with context around users, interface, data, and business logic, one will bubble up higher than the other. It’s an exciting time to be UI engineer. There’s a shift in progress. We’re at the beginning of it. I enjoy being part of it.
+    </p>
+  </Layout>
+)
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
